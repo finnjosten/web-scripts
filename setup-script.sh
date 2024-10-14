a945dbb47a568999dcd06322a095d9a76370d7ff
Black Sparow <117974776+BlackSparowYT@users.noreply.github.com>
2024-10-14 16:56:35 +0200

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -22,6 +22,13 @@ ask_for_input "What do you want to create (laravel/react/docker/plain)? " projec
 # Ask for GitHub repo link (optional)
 ask_for_input "Enter the GitHub repo link (leave empty if not applicable): " repo_link
 
+# Ask if the GitHub link should be used as a template
+if [[ -n "$repo_link" ]]; then
+    ask_for_input "Should this GitHub link be used as a template? (yes/no): " use_as_template
+else
+    use_as_template="no"
+fi
+
 # Ask if it's a static site (only for plain site)
 if [[ "$project_type" == "plain" ]]; then
     ask_for_input "Is this a static/plain site? (default: yes) [yes/no]: " is_static
@@ -85,8 +92,23 @@ if [ -z "$skip_setup" ]; then
     case "$project_type" in
         laravel)
             if [ -n "$repo_link" ]; then
-                # Install Laravel dependencies from GitHub
-                composer install
+                if [[ "$use_as_template" == "yes" ]]; then
+                    # Clone the custom Laravel 11 template
+                    git clone "$repo_link" "$domain_dir"
+                    cd "$domain_dir" || exit
+    
+                    # Remove the git history to make it a new repository
+                    rm -rf .git
+                    git init
+                    git branch -m main
+                    # Install Laravel dependencies
+                    composer install
+                else
+                    # Install Laravel dependencies from GitHub
+                    git clone "$repo_link" "$domain_dir"
+                    cd "$domain_dir" || exit
+                    composer install
+                fi
 
                 if [ ! -f ".env" ]; then
                     echo -e "\e[31mYou will need to upload your .env file !!\e[0m"
@@ -100,7 +122,7 @@ if [ -z "$skip_setup" ]; then
                 sudo chown -R www-data:www-data "$domain_dir"/storage "$domain_dir"/bootstrap/cache
             else
                 # Ask user to choose setup option
-                ask_for_input "No GitHub link provided. Choose setup option: (1) Plain Laravel, (2) Custom Laravel 11 template, (3) Nothing: " setup_option
+                ask_for_input "No GitHub link provided. Choose setup option: (1) Plain Laravel, (2) Nothing: " setup_option
 
                 case "$setup_option" in
                     1)
@@ -125,36 +147,12 @@ if [ -z "$skip_setup" ]; then
                         ;;
     
                     2)
-                        # Clone the custom Laravel 11 template
-                        git clone https://github.com/BlackSparowYT/laravel11-template "$domain_dir"
-                        cd "$domain_dir" || exit
-    
-                        # Remove the git history to make it a new repository
-                        rm -rf .git
-                        git init
-                        git branch -m main
-                        # Install Laravel dependencies from GitHub
-                        composer install
-
-                        if [ ! -f ".env" ]; then
-                            echo -e "\e[31mYou will need to upload your .env file !!\e[0m"
-                            echo -e "\e[31mYou will need to manually run 'php artisan key:generate' !!\e[0m"
-                            echo -e "\e[33m5. The script will proceed in 5 sec.\e[0m"
-                            sleep 5
-                        else
-                            php artisan key:generate
-                        fi
-                        php artisan storage:link
-                        sudo chown -R www-data:www-data "$domain_dir"/storage "$domain_dir"/bootstrap/cache
-                        ;;
-    
-                    3)
                         echo -e "\e[33mYou have not provided a GitHub repo or chosen to set up a default installation, please manually upload the files and do the following:\e[0m"
-                        echo -e "\e[33m1. - Run 'composer install' to install the dependencies.\e[0m"
-                        echo -e "\e[33m2. - Run 'php artisan key:generate' to generate the application key.\e[0m"
-                        echo -e "\e[33m3. - Run 'php artisan storage:link' to create the storage symlink.\e[0m"
-                        echo -e "\e[33m4. - Run 'sudo chown -R www-data:www-data $domain_dir/storage $domain_dir/bootstrap/cache' to set up the correct permissions.\e[0m"
-                        echo -e "\e[33m5. The script will proceed in 10 sec to generate an nginx config file and try to set up SSL.\e[0m"
+                        echo -e "\e[33m1. Run 'composer install' to install the dependencies.\e[0m"
+                        echo -e "\e[33m2. Run 'php artisan key:generate' to generate the application key.\e[0m"
+                        echo -e "\e[33m3. Run 'php artisan storage:link' to create the storage symlink.\e[0m"
+                        echo -e "\e[33m4. Run 'sudo chown -R www-data:www-data $domain_dir/storage $domain_dir/bootstrap/cache' to set up the correct permissions.\e[0m"
+                        echo -e "\e[33mThe script will proceed in 10 sec to generate an nginx config file and try to set up SSL.\e[0m"
                         sleep 10
                         ;;
                     *)
@@ -166,42 +164,109 @@ if [ -z "$skip_setup" ]; then
             ;;
 
         react)
+            # Ask if a GitHub repo link is provided
             if [ -n "$repo_link" ]; then
-                # Install React dependencies from GitHub
-                npm install
-                npm run build
-    
-                if [ ! -d "dist" ]; then
-                    echo -e "\e[31m'dist' folder not found. Build may have failed. Exiting.\e[0m"
-                    exit 1
+                if [[ "$use_as_template" == "yes" ]]; then
+                    # Clone the React template from GitHub
+                    git clone "$repo_link" "$domain_dir"
+                    cd "$domain_dir" || exit
+
+                    # Remove the git history to make it a new repository
+                    rm -rf .git
+                    git init
+                    git branch -m main
+                    
+                    # Install React dependencies
+                    npm install
+                    
+                    # Build the project
+                    npm run build
+
+                    # Check if the 'dist' folder exists
+                    if [ ! -d "dist" ]; then
+                        echo -e "\e[31m'dist' folder not found. Build may have failed. Exiting.\e[0m"
+                        echo -e "\e[31mPlease check why the build fails and rerun the script or continue manually.\e[0m"
+                        exit 1
+                    fi
+                else
+                    # Clone the React project from GitHub
+                    git clone "$repo_link" "$domain_dir"
+                    cd "$domain_dir" || exit
+                    
+                    # Install React dependencies
+                    npm install
+                    
+                    # Build the project
+                    npm run build
+
+                    # Check if the 'dist' folder exists
+                    if [ ! -d "dist" ]; then
+                        echo -e "\e[31m'dist' folder not found. Build may have failed. Exiting.\e[0m"
+                        echo -e "\e[31mPlease check why the build fails and rerun the script or continue manually.\e[0m"
+                        exit 1
+                    fi
                 fi
             else
-                # Ask if user wants a default React setup
+                # No GitHub link provided, ask if the user wants a boilerplate
                 ask_for_input "No GitHub link provided. Would you like to set up a default React installation? (yes/no): " setup_default
+
                 if [[ "$setup_default" == "yes" || "$setup_default" == "y" ]]; then
                     # Download default React setup
                     npx create-react-app "$domain_dir"
                     cd "$domain_dir" || exit
+                    
+                    # Install dependencies and build
                     npm run build
+                    
+                    # Check if the 'dist' folder exists
+                    if [ ! -d "dist" ]; then
+                        echo -e "\e[31m'dist' folder not found. Build may have failed. Exiting.\e[0m"
+                        echo -e "\e[31mPlease check why the build fails and rerun the script or continue manually.\e[0m"
+                        exit 1
+                    fi
                 else
                     echo -e "\e[33mYou have not provided a GitHub repo or chosen to set up a default installation. Please manually upload the files and do the following:\e[0m"
                     echo -e "\e[33m1. Run 'npm install' to install the dependencies.\e[0m"
                     echo -e "\e[33m2. Run 'npm run build' to build the project.\e[0m"
-                    echo -e "\e[33m3. The script will proceed in 10 seconds to generate an Nginx config file and try to set up SSL.\e[0m"
+                    echo -e "\e[33mThe script will proceed in 10 seconds to generate an Nginx config file and try to set up SSL.\e[0m"
                     sleep 10
                 fi
             fi
-            ;;
 
         docker)
-            ask_for_input "Enter the port the container is reachable on: " port
+            if [ -n "$repo_link" ]; then
+                if [[ "$use_as_template" == "yes" ]]; then
+                    # Clone the Docker template
+                    git clone "$repo_link" "$domain_dir"
+                    cd "$domain_dir" || exit
+                    # Add any specific Docker setup commands if necessary
+                else
+                    # Install Docker dependencies from GitHub
+                    git clone "$repo_link" "$domain_dir"
+                    cd "$domain_dir" || exit
+                fi
+            else
+                echo -e "\e[31mPlease provide a GitHub link to set up Docker project.\e[0m"
+                exit 1
+            fi
             ;;
-        
+
         plain)
-            # No special setup for plain/static sites
+            if [[ "$is_static" == "yes" ]]; then
+                # Setup static files if it's a static site
+                echo -e "\e[32mSetting up static site files in $domain_dir.\e[0m"
+                # Add commands to create or copy static files as necessary
+            else
+                echo -e "\e[32mSetting up plain dynamic site files in $domain_dir.\e[0m"
+                # Add commands to create or copy dynamic files as necessary
+            fi
+            ;;
+
+        *)
+            echo -e "\e[31mInvalid project type selected. Exiting...\e[0m"
+            exit 1
             ;;
     esac
-fi
 
 
 # Create nginx config file
